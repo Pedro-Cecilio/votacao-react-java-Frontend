372bed6911c562be8afeb57b2e31c9e0f810c415
Pedro-Cecilio <cardev604@gmail.com>
2024-04-11 16:51:25 -0300

    :sparkles: feat: Inserindo tema de cores

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ChakraProvider } from '@chakra-ui/react'
 import Routes from './routes/Routes.tsx'
+import tema from './temas/temas.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={tema}>
       <Routes/>
     </ChakraProvider>
   </React.StrictMode>,
