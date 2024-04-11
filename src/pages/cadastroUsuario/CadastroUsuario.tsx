790283e6db73802e6600e1151544e5b19ec444de
Pedro-Cecilio <cardev604@gmail.com>
2024-04-11 16:53:24 -0300

    :sparkles: feat: Construindo tela com formulário de cadastro
    Closes #3

--- src/pages/cadastroUsuario/CadastroUsuario.tsx
+++ src/pages/cadastroUsuario/CadastroUsuario.tsx
@@ -1,11 +1,101 @@
+import { Button, Flex, FormControl, FormLabel, Heading, Input, InputGroup, useToast } from "@chakra-ui/react";
+import { SubmitErrorHandler, useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 
+const inputSchema = z.object({
+    email: z.string().email("Email deve ter formato válido."),
+    senha: z.string().min(8, "Senha deve conter no mínimo 8 caracteres."),
+    nome: z.string().min(3, "Nome deve conter no mínimo 3 caracteres.").max(20, "Nome deve conter no máximo 20 caracteres."),
+    sobrenome: z.string().min(2, "Sobrenome deve conter no mínimo 2 caracteres.").max(20, "Sobrenome deve conter no máximo 20 caracteres."),
+    cpf: z.string().length(11, "Cpf deve ter exatamente 11 caracteres")
+})
 
-const CadastroUsuario = ()=>{
+type inputs = z.infer<typeof inputSchema>
+
+const CadastroUsuario = () => {
+    const toast = useToast();
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<inputs>({
+        resolver: zodResolver(inputSchema)
+    })
+
+    const onSubmit = (data: inputs) => {
+        console.log(errors)
+        console.log(data)
+    }
+
+    const onError: SubmitErrorHandler<inputs> = (errors) => {
+        for (const [_, valor] of Object.entries(errors)) {
+            if (valor) {
+                toast({
+                    title: "Erro",
+                    description: valor.message,
+                    status: "error",
+                    duration: 3000,
+                    isClosable: true,
+                    position: "top-right"
+                })
+                break;
+            }
+        }
+    };
+
     return (
-        <div>
-            <h1>Cadastro de Usuário</h1>
-        </div>
+        <Flex
+            gap={12}
+            backgroundColor={'cinza.700'}
+            flexDirection={'column'}
+            alignItems={'center'}
+            justifyContent={'center'}
+            textColor={'branco'}
+            minH='100vh'
+        >
+            <Heading as={'h1'} size={'xl'}>Cadastro de Usuário</Heading>
+            <form
+                style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
+                onSubmit={handleSubmit(onSubmit, onError)}
+            >
+                <FormControl
+                    display={'flex'}
+                    flexDirection={'column'}
+                    alignItems={'center'}
+                >
+                    <InputGroup
+                        gap={2}
+                        display={'flex'}
+                        flexDirection={'column'}
+                        maxWidth={500}
+                        minWidth={300}
+                    >
+                        <FormLabel>Email</FormLabel>
+                        <Input type='email'  {...register('email')} data-testid="input-email" />
+                        <FormLabel>Senha</FormLabel>
+                        <Input type='password' {...register('senha')} data-testid="input-senha" />
+                        <FormLabel>Nome</FormLabel>
+                        <Input type='text' {...register('nome')} data-testid="input-nome" />
+                        <FormLabel>Sobrenome</FormLabel>
+                        <Input type='text' {...register('sobrenome')} data-testid="input-sobrenome" />
+                        <FormLabel>Cpf</FormLabel>
+                        <Input type='text' {...register('cpf')} data-testid="input-cpf" />
+                    </InputGroup>
+                </FormControl>
+                <Button
+                    my={4}
+                    maxWidth={300}
+                    minWidth={200}
+                    type="submit"
+                    colorScheme='blue'>
+                    Enviar
+                </Button>
+            </form>
+        </Flex>
+
     );
 }
 
