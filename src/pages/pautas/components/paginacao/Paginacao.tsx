16e472b151095bd64cb1f1d451a4ae0039606085
Pedro-Cecilio <cardev604@gmail.com>
2024-04-16 20:28:41 -0300

    :sparkles: feat: Construindo componente para paginação

---
+++ src/pages/pautas/components/paginacao/Paginacao.tsx
@@ -0,0 +1,43 @@
+import { Flex } from "@chakra-ui/react";
+import Botao from "../../../components/botao/Botao";
+interface PaginacaoProps {
+    readonly paginaAtual: number;
+    readonly totalPaginas: number;
+    readonly controlarPaginaAtual: (paginaAtual: number) => void;
+}
+function Paginacao({ paginaAtual, totalPaginas, controlarPaginaAtual }: PaginacaoProps) {
+    const paginaAnterior = () => {
+        if (paginaAtual > 0) {
+            controlarPaginaAtual(paginaAtual - 1);
+        }
+    };
+
+    const proximaPagina = () => {
+        if (paginaAtual < totalPaginas - 1) {
+            controlarPaginaAtual(paginaAtual + 1);
+        }
+    };
+
+    return (
+
+        <Flex justifyContent="center" gap={8}>
+            {paginaAtual > 0 && (
+                <Botao
+                    onClick={paginaAnterior}
+                    texto="Anterior"
+                    tamanho={"sm"}
+                />
+            )}
+            {paginaAtual < totalPaginas - 1 && (
+                <Botao
+                    onClick={proximaPagina}
+                    texto="Próxima"
+                    tamanho={"sm"}
+                />
+            )}
+        </Flex>
+
+    );
+}
+
+export default Paginacao;
