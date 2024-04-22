dc69ee7b4ff0b2d33723163eb39a0c4828da7dea
Pedro-Cecilio <cardev604@gmail.com>
2024-04-22 19:40:32 -0300

    :recycle: refactor: modificando método de buscar usuario por cpf para validar existencia do usuario por cpf

--- src/services/buscarUsuarioPorCpf.service.ts
+++
@@ -1,6 +0,0 @@
-import { api } from "../lib/api"
-
-export const buscarUsuarioPorCpfService = async (cpf: string) => {
-    const resposta = await api.get<UsuarioResposta>(`/usuario?cpf=${cpf}`, );
-    return resposta;
-}
---
+++ src/services/verificarSeUsuarioExistePorCpf.service.ts
@@ -0,0 +1,6 @@
+import { api } from "../lib/api"
+
+export const verificarSeUsuarioExistePorCpfService = async (cpf: string) => {
+    const resposta = await api.get<VerificarSeUsuarioExisteResposta>(`/usuario/existe?cpf=${cpf}`, );
+    return resposta;
+}
