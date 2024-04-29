99414ecd951aa7b367cbc97b5ce083a15aab01b7
Pedro-Cecilio <cardev604@gmail.com>
2024-04-28 22:09:01 -0300

    :test_tube: test: Testes para página Detalhes

--- src/tests/__mocks__/useBuscarDetalhesPautaMock.ts
+++ src/tests/__mocks__/useBuscarDetalhesPautaMock.ts
@@ -1,16 +1,47 @@
-import * as useCriarUsuario from "../../hooks/useCriarUsuario"
+import { Categoria } from "../../enums/categoria";
+import * as useBuscarDetalhesPauta from "../../hooks/useBuscarDetalhesPauta"
+import { DetalhesPautaDados } from "../../models/pautaModels";
+import { StatusSessaoVotacao } from "../../models/sessaoVotacaoModels";
 
 
-export const useCriarUsuarioMock = (jestfn: jest.Mock<any, any, any>)=>{
-    const criarUsuarioResposta: CriarUsuarioResposta = {
-        id: 1,
-        email: "novousuario@email.com",
-        nome: "João",
-        sobrenome: "Silva",
-        cpf: "12345678900",
-        admin: false
+export const useDetalhesPautaMock = () => {
+    const dataAbertura = new Date();
+    const dataFechamento = new Date();
+    dataFechamento.setMinutes(dataAbertura.getMinutes() + 10);
+
+    const detalhesPautaDados: DetalhesPautaDados = {
+        dadosPauta: {
+            id: 1,
+            assunto: "Você sabe dirigir?",
+            categoria: Categoria.TRANSPORTE,
+            usuario: {
+                id: 1,
+                nome: "João",
+                sobrenome: "Silva",
+                cpf: "12345678900",
+                admin: false
+            },
+            sessaoVotacao: {
+                id: 1,
+                pauta_id: 1,
+                sessaoAtiva: true,
+                votosPositivos: 0,
+                votosNegativos: 0,
+                dataAbertura: dataAbertura.toISOString(),
+                dataFechamento: dataFechamento.toISOString()
+            },
+        },
+        status: StatusSessaoVotacao.EM_ANDAMENTO
     }
-    jest.spyOn(useCriarUsuario, 'useCriarUsuario').mockReturnValue({
-        criarUsuario: jestfn.mockResolvedValue(criarUsuarioResposta)
-    });
+    const useBuscarDetalhesPautaMock = (jestfn: jest.Mock<any, any, any>) => {
+        jest.spyOn(useBuscarDetalhesPauta, 'useBuscarDetalhesPauta').mockReturnValue({
+            buscarDetalhesPauta: jestfn.mockResolvedValue(detalhesPautaDados)
+        });
+    }
+    
+    return {
+        useBuscarDetalhesPautaMock,
+        detalhesPautaDados
+    }
+
 }
