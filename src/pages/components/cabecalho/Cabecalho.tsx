f5866e1ddefaffef58751e7a34473e7b87349cd8
Pedro-Cecilio <cardev604@gmail.com>
2024-04-30 18:39:39 -0300

    :test_tube: Testando componente de Cabecalho

--- src/pages/components/cabecalho/Cabecalho.tsx
+++ src/pages/components/cabecalho/Cabecalho.tsx
@@ -1,4 +1,4 @@
-import { Flex, Image, Link as ChakraLink, Menu, MenuButton, MenuList, IconButton, MenuItem, useBreakpointValue  } from "@chakra-ui/react"
+import { Flex, Image, Link as ChakraLink, Menu, MenuButton, MenuList, IconButton, MenuItem, useBreakpointValue } from "@chakra-ui/react"
 import logo from "../../../assets/logo.svg"
 import { Link as ReactRouterLink, useNavigate } from "react-router-dom"
 import { useDadosUsuarioStore } from "../../../hooks/useDadosUsuarioStore"
@@ -7,16 +7,19 @@ import { useTokenLocalStorage } from "../../../hooks/useTokenLocalStorage";
 
 const Cabecalho = () => {
     const { admin } = useDadosUsuarioStore()
-    const mostrarMenuBurger = useBreakpointValue({ base: true, md: false });
+    const mostrarMenuBurger = useBreakpointValue<boolean>({ base: true, md: false }, {
+        ssr: false
+    });
     const navigate = useNavigate();
-    const {removerTokenDoLocalStorage} = useTokenLocalStorage();
-    const logout = ()=>{
+    const { removerTokenDoLocalStorage } = useTokenLocalStorage();
+    const logout = () => {
         removerTokenDoLocalStorage();
         window.location.reload()
     }
+
     return (
         <Flex as="header" bg={'cinza.700'} p={3} justifyContent={"space-between"} alignItems={"center"} textColor={"branco"}>
-            <Image src={logo} w='50px' />
+            <Image src={logo} w='50px' data-testid={"logo-cabecalho"} />
             {mostrarMenuBurger ? (
                 <Menu>
                     <MenuButton
@@ -55,15 +58,15 @@ const Cabecalho = () => {
                 :
 
                 <Flex gap={6}>
-                    <ChakraLink as={ReactRouterLink} to={"/explorar"} fontWeight={"bold"} m={1}>Explorar</ChakraLink >
+                    <ChakraLink as={ReactRouterLink} to={"/explorar"} fontWeight={"bold"} m={1} data-testid={"nav-explorar"}>Explorar</ChakraLink >
                     {admin &&
                         <>
-                            <ChakraLink as={ReactRouterLink} to={"/cadastro"} fontWeight={"bold"} m={1}>Novo usuário</ChakraLink >
-                            <ChakraLink as={ReactRouterLink} to={"/minhasPautas"} fontWeight={"bold"} m={1}>Minhas pautas</ChakraLink >
+                            <ChakraLink as={ReactRouterLink} to={"/cadastro"} fontWeight={"bold"} m={1} data-testid={"nav-novo-usuario"}>Novo usuário</ChakraLink >
+                            <ChakraLink as={ReactRouterLink} to={"/minhasPautas"} fontWeight={"bold"} m={1} data-testid={"nav-minhas-pautas"}>Minhas pautas</ChakraLink >
                         </>
 
                     }
-                    <ChakraLink as={ReactRouterLink}  fontWeight={"bold"} m={1} onClick={logout}>Sair</ChakraLink >
+                    <ChakraLink as={ReactRouterLink} fontWeight={"bold"} m={1} onClick={logout} data-testid={"nav-logout"}>Sair</ChakraLink >
                 </Flex>
 
             }
